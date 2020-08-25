85fb4c00a02c8e6d34c97f30a975edc7c12e6dcc
phuongnho2011 <phuongnho296@gmail.com>
2020-08-25 13:58:22 +0700

    basis

---
+++ include/my_robot_core_config.h
@@ -0,0 +1,142 @@
+#include <Arduino.h>
+#include <ros.h>
+#include <ros/time.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/String.h>
+#include <std_msgs/UInt16.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/JointState.h>\
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Twist.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <my_imu.h>
+
+// /* Time update index */
+// #define CONTROL_MOTOR_TIME_INDEX                0       /*!< Time index control motor */
+// #define CMD_VEL_PUBLISH_TIME_INDEX              1       /*!< Time index publish velocity */
+// #define DRIVE_INFORMATION_PUBLISH_TIME_INDEX    2       /*!< Time index publish drive information */
+// #define IMU_PUBLISH_TIME_INDEX                  3       /*!< Time index publish IMU information */
+// #define LOG_PUBLISH_TIME_INDEX                  5
+// #define CONTROL_MOTOR_TIMEOUT_TIME_INDEX        6       /*!< Time index control motor timeout */
+
+// /* Frequency of publish/subscribe */
+// #define CONTROL_MOTOR_SPEED_FREQUENCY          10       /*!< Frequency in Hz to control motor */
+// #define CONTROL_MOTOR_TIMEOUT                  500      /*!< Period in ms to check control motor timeout */
+// #define IMU_PUBLISH_FREQUENCY                  15      	/*!< Frequency in Hz to publish IMU information */
+// #define CMD_VEL_PUBLISH_FREQUENCY              5       	/*!< Frequency in Hz to publish robot velocity */
+// #define DRIVE_INFORMATION_PUBLISH_FREQUENCY    5       	/*!< Frequency in Hz to publish drive information */
+// #define DEBUG_LOG_FREQUENCY                    10       /*!< Frequency in Hz to send log debug messages */
+
+// /* Linear & Angular velocity index */
+// #define WHEEL_NUM       2                       /*!< Num wheel */
+
+// #define LEFT            0                       /*!< Left wheel index */
+// #define RIGHT           1                       /*!< Right wheel index */
+
+// #define LINEAR          0                       /*!< Linear velocity index */
+// #define ANGULAR         1                       /*!< Angular velocity index */
+
+// void commandVelocityCallback(const geometry_msgs::Twist& cmd_vel_msg);
+// void resetCallback(const std_msgs::Empty& reset_msg);
+// void publishCmdVelFromAMEGA(void);
+
+// ros::Time rosNow(void);
+
+// void updateVariable(bool isConnected);
+// void updateMotorInfo(int32_t left_tick, int32_t right_tick);
+// void updateTime(void);
+// void updateOdometry(void);
+// void updateJoint(void);
+// void updateTF(geometry_msgs::TransformStamped& odom_tf);
+// void updateGyroCali(bool isConnected);
+// void updateGoalVelocity(void);
+// void updateTFPrefix(bool isConnected);
+
+// void initOdom(void);
+// void initJointStates(void);
+
+// bool calcOdometry(double diff_time);
+
+// void sendLogMsg(void);
+// void waitForSerialLink(bool isConnected);
+
+// ros::NodeHandle nh;
+// ros::Time current_time;
+// uint32_t current_offset;
+
+// ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", commandVelocityCallback);
+
+// // Command velocity of Turtlebot3 using RC100 remote controller
+// geometry_msgs::Twist cmd_vel_amega_msg;
+// ros::Publisher cmd_vel_amega_pub("cmd_vel_amega", &cmd_vel_amega_msg);
+
+// // Odometry of Turtlebot3
+// nav_msgs::Odometry odom;
+// ros::Publisher odom_pub("odom", &odom);
+
+// // Joint(Dynamixel) state of Turtlebot3
+// sensor_msgs::JointState joint_states;
+// ros::Publisher joint_states_pub("joint_states", &joint_states);
+
+// /*******************************************************************************
+// * Transform Broadcaster
+// *******************************************************************************/
+// // TF of Turtlebot3
+// geometry_msgs::TransformStamped odom_tf;
+// tf::TransformBroadcaster tf_broadcaster;
+
+// /*******************************************************************************
+// * SoftwareTimer of Turtlebot3
+// *******************************************************************************/
+// static uint32_t tTime[10];
+
+// /*******************************************************************************
+// * Calculation for odometry
+// *******************************************************************************/
+// bool init_encoder = true;
+// int32_t last_diff_tick[WHEEL_NUM] = {0, 0};
+// double  last_rad[WHEEL_NUM]       = {0.0, 0.0};
+
+// /*******************************************************************************
+// * Update Joint State
+// *******************************************************************************/
+// double  last_velocity[WHEEL_NUM]  = {0.0, 0.0};
+
+// /*******************************************************************************
+// * Declaration for SLAM and navigation
+// *******************************************************************************/
+// unsigned long prev_update_time;
+// float odom_pose[3];
+// double odom_vel[3];
+
+// /*******************************************************************************
+// * ROS Parameter
+// *******************************************************************************/
+// char get_prefix[10];
+// char* get_tf_prefix = get_prefix;
+
+// char odom_header_frame_id[30];
+// char odom_child_frame_id[30];
+
+// char imu_frame_id[30];
+
+// char joint_state_header_frame_id[30];
+
+// /*******************************************************************************
+// * Declaration for controllers
+// *******************************************************************************/
+// float zero_velocity[WHEEL_NUM] = {0.0, 0.0};
+// float goal_velocity[WHEEL_NUM] = {0.0, 0.0};
+// float goal_velocity_from_button[WHEEL_NUM] = {0.0, 0.0};
+// float goal_velocity_from_cmd[WHEEL_NUM] = {0.0, 0.0};
+// float goal_velocity_from_rc100[WHEEL_NUM] = {0.0, 0.0};
+
+// #define MIN_LINEAR_VELOCITY -2 
+// #define MAX_LINEAR_VELOCITY  2
+
+// #define MIN_ANGULAR_VELOCITY -1
+// #define MAX_ANGULAR_VELOCITY 1
--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,30 @@
 #include <Arduino.h>
-#include <ros.h>
-#include <ros/time.h>
-#include <tf/transform_broadcaster.h>
+#include <my_robot_core_config.h>
 
-ros::NodeHandle  nh;
+//  init_motor
 
-geometry_msgs::TransformStamped t ;
-tf::TransformBroadcaster broadcaster;
+//  init_encoder
 
-char base_link[] = "/base_link";
-char odom[] = "/odom";
+//  init_imu
+    my_imu mpu(0x68);
+
+//  ros_setup
+
 
 void setup()
 {
-  nh.initNode();
-  broadcaster.init(nh);
+    Serial.begin(19200);
+    mpu.imu_init();
+    mpu.calculate_IMU_error();
+   
 }
 
 void loop()
 {
-  t.header.frame_id = odom;
-  t.child_frame_id = base_link;
-  t.transform.translation.x = 1.0;
-  t.transform.rotation.x = 0.0;
-  t.transform.rotation.y = 0.0;
-  t.transform.rotation.z = 0.0;
-  t.transform.rotation.w = 1.0;
-  t.header.stamp = nh.now();
-  broadcaster.sendTransform(t);
-  nh.spinOnce();
-  delay(10);
+    mpu.calculateIMU();
+    Serial.print(mpu.getVelX());
+    Serial.print("/");
+    Serial.print(mpu.getVelY());
+    Serial.print("/");
+    Serial.println(mpu.getyaw());
 }
-// #include <ros.h>
-// #include <std_msgs/Empty.h>
-// #include <nav_msgs/Odometry.h>
-
-// ros::NodeHandle nh;
-
-// nav_msgs::Odometry odom_msg;
-// ros::Publisher odom("odom", &odom_msg);
-
-// unsigned long timer;
-
-
-// void setup()
-// {
-//   nh.initNode();
-//   nh.advertise(odom);
-  
-// }
-
-// void loop()
-// {
-//   odom_msg.pose.pose.position.x = 0.0;
-//   odom_msg.pose.pose.position.y = 0.0;
-//   odom_msg.pose.pose.position.z = 0.0;
-
-//   odom_msg.pose.pose.orientation.x = 1.0;
-//   odom_msg.pose.pose.orientation.y = 0.0;
-//   odom_msg.pose.pose.orientation.z = 0.0;
-//   odom_msg.pose.pose.orientation.w = 0.0;
-
-//   odom_msg.twist.twist.linear.x  = 0.0;
-//   odom_msg.twist.twist.angular.z = 0.0;
-//   unsigned long currentMillis = millis();
-//   if(currentMillis - timer >= 50)
-//   {
-//     timer = currentMillis + 50;
-//     odom.publish(&odom_msg);
-//   }
-//   nh.spinOnce();
-//   delay(1000);
-// }
-
