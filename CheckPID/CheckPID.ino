afa024cd8ee30b5f05755ad37c689b72b7323e8f
phuongnho2011 <phuongnho296@gmail.com>
2020-12-11 13:33:28 +0700

    BUG LUC

---
+++ CheckPID/CheckPID.ino
@@ -0,0 +1,34 @@
+#include <my_motor_driver.h>
+
+motor_driver motor;
+float value[2] = {0.5,1};
+
+void setup()
+{
+  motor.init();
+  Serial.begin(9600);
+}
+
+long t = millis();
+
+void loop()
+{
+  Serial.println(motor.getLeftencoder());
+  //if(millis() - t > 100){
+  //motor.PID(millis() - t);
+  //motor.control_Motor(0.033,0.287,value,millis() - t);
+  //Serial.println(motor.getRightencoder());
+  //Serial.println(motor.getSpeedL());
+  //t = millis();
+  //}
+}
+
+void motor_driver::cal_encoderL()
+{
+  motor.read_EncoderL();
+}
+
+void motor_driver::cal_encoderR()
+{
+  motor.read_EncoderR();
+}
