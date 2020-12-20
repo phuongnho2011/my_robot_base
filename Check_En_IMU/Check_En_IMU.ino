801065a1c08ffa9e9265c7c97963d01771c79a5d
phuongnho2011 <phuongnho296@gmail.com>
2020-12-20 21:12:21 +0700

    bug luc

--- Check_En_IMU/Check_En_IMU.ino
+++ Check_En_IMU/Check_En_IMU.ino
@@ -1,16 +1,27 @@
- #include <my_imu2.h>
+#include <MPU9250.h>
 #include <my_motor_driver.h>
 
-my_imu imu(0x68);
+MPU9250 mpu;
 
 void setup() {
   Serial.begin(115200);
-  imu.init();
-  imu.calculate_IMU_error();
+  Wire.begin();
+  delay(2000);
+
+  mpu.setup(0x68); 
+
+  delay(5000);
+
+  mpu.calibrateAccelGyro();
+  mpu.calibrateMag();
+
+  mpu.printCalibration();
 }
 
 void loop() {
-  imu.calculateIMU();
-  //Serial.println(imu.getGyroZerror());
-  Serial.println(imu.getgyroZangle());
+  if(mpu.update()) {
+ // Serial.println(atan2f(mpu.getQuaternionX() * mpu.getQuaternionW() + mpu.getQuaternionY() * mpu.getQuaternionZ(),
+ //               0.5f - mpu.getQuaternionZ() * mpu.getQuaternionZ() - mpu.getQuaternionW() * mpu.getQuaternionW()));
+    Serial.println(mpu.getYaw());
+  }
 }
