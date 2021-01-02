e2a79a46f9258ea83fbaef013fd23202e28426bd
phuongnho2011 <phuongnho296@gmail.com>
2021-01-02 11:39:11 +0700

    kiet

--- my_robot_mega/my_robot_mega.ino
+++ my_robot_mega/my_robot_mega.ino
@@ -380,14 +380,14 @@ bool calcOdometry(double diff_time)
   theta = yaw * PI / 180;
   delta_theta = theta - last_theta;
 
-  w = delta_theta / step_time;
-  //w = (-mt_driver.getSpeedFL() + mt_driver.getSpeedFR() - mt_driver.getSpeedBL() + mt_driver.getSpeedBR())/radtorpm*(WHEEL_RADIUS/(4*(WHEEL_SEPARATION_WIDTH+WHEEL_SEPARATION_LENGTH)));
+  //w = delta_theta / step_time;
+  w = (-mt_driver.getSpeedFL() + mt_driver.getSpeedFR() - mt_driver.getSpeedBL() + mt_driver.getSpeedBR())/radtorpm*2*PI*WHEEL_RADIUS/60*(WHEEL_RADIUS/(4*(WHEEL_SEPARATION_WIDTH+WHEEL_SEPARATION_LENGTH)));
   vx = (mt_driver.getSpeedFL() + mt_driver.getSpeedFR() + mt_driver.getSpeedBL() + mt_driver.getSpeedBR())/radtorpm*2*PI*WHEEL_RADIUS/60*(WHEEL_RADIUS/4);
   vy = (- mt_driver.getSpeedFL() + mt_driver.getSpeedFR() + mt_driver.getSpeedBL() - mt_driver.getSpeedBR())/radtorpm*2*PI*WHEEL_RADIUS/60*(WHEEL_RADIUS/4);
 
   delta_x = (vx * cos(w) - vy * sin(w)) * step_time;
   delta_y = (vx * sin(w) + vy * cos(w)) * step_time;
-  // delta_theta = w*step_time;
+  delta_theta = w*step_time;
 
   odom_pose[0] += delta_x;
   odom_pose[1] += delta_y;
